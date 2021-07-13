52de113eeaf86bc728dd41f014d745d91f02ba28
Santiago Oyarbide Fernandez <63980929+Sfoyarbide@users.noreply.github.com>
2021-07-13 13:03:19 -0300

    Improving bot

--- Main.py
+++ Main.py
@@ -8,26 +8,28 @@ import asyncio
 
 client = discord.Client()
 
-Alone_words = [
-  "solo", "Solo", "Alone", "alone"
-]
-Alone_Answers = [
-  "So, are you alone?. God You are Pathetic.", 
-  "Alone aHa, God You are Pathetic.",
-  "Hahaha Alone, God You are Pathetic."
-]
 
+activeMessages = False
 idChannel = 1
 
 Today = datetime.datetime.today().weekday()
 
 async def AloneOnSaturday():
   await client.wait_until_ready()
-  
+  #Global
+  global activeMessages
   global idChannel
+
   while not client.is_closed():
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    if Today == 0 and current_time == "01:00:00": # Day: 0, 01:00:00, Saturday in Argentina
+
+    # Debug Logs
+    #channel = client.get_channel(id=idChannel)
+    #print(current_time)
+    #print(idChannel)
+    #print(activeMessages)
+    
+    if Today == 6 and current_time == "01:00:00": # Day: 0, 01:00:00
       await idChannel.send(file=discord.File("AloneBot.png"))
     await asyncio.sleep(1)
 
@@ -37,27 +39,60 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  global activeMessages
   global idChannel
+
   if message.author == client.user:
     return
 
   mgs = message.content
 
-  if message.content.startswith('%Saturday'):
+  if message.content.startswith('%saturday'):
     await message.channel.send(file=discord.File("AloneBot.png"))
-  
+
+  if message.content.startswith('%activeMessages'):
+      if activeMessages == False:
+        activeMessages = True
+        await message.channel.send("I active it!")
+      elif activeMessages == True:
+        activeMessages = False
+        await message.channel.send("I deactive it!")
+
+  if message.content.startswith('%help'):
+    await message.channel.send("You can use this commads:")
+    await message.channel.send("1: %saturday")
+    await message.channel.send("2: %activeMessages")
+    #ask time left to saturday 
+
   if message.content.startswith('%general'):
     idChannel = message.channel
     print(idChannel)
     await message.channel.send("Nice, I got it!")
 
-  if any(word in mgs for word in Alone_words):
-    await message.channel.send(random.choice(Alone_Answers))
+  if activeMessages == True:
+    if any(word in mgs for word in Alone_words):
+      await message.channel.send(random.choice(Alone_Answers))
 
 @client.event
 async def on_message_delete(message):
+  global activeMessages
   global idChannel
-  await message.channel.send("Ha are you deleting a message?. God you are Pathetic.")
 
+  print(message.author)
+  print(activeMessages)
+
+  if activeMessages == True:
+    if message.author == "Rythm#3722":
+      return
+    if message.author == "Rythm 2#2000":
+      return
+    if message.author == "Dank Memer#5192":
+       return
+    if message.author == "Groovy#7254":
+      return
+    else:
+      await message.channel.send("Ha are you deleting a message?. God you are Pathetic.")
+
+keep_alive()
 client.loop.create_task(AloneOnSaturday())
-client.run(Token)
+client.run() #Token
