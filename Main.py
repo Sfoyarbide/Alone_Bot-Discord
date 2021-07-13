4dfeb25dc923b5e0b4e064a8563e20ae630119b6
Santiago Oyarbide Fernandez <63980929+Sfoyarbide@users.noreply.github.com>
2021-07-13 00:24:52 -0300

    Update Main.py
    Dinamic "general"!

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import random
 import datetime
 import asyncio
 
-Token = os.environ['TOKEN']
+#Token
 
 client = discord.Client()
 
@@ -17,16 +17,18 @@ Alone_Answers = [
   "Hahaha Alone, God You are Pathetic."
 ]
 
+idChannel = 1
+
 Today = datetime.datetime.today().weekday()
 
 async def AloneOnSaturday():
   await client.wait_until_ready()
-  channel = client.get_channel() # Put id channel
+  
+  global idChannel
   while not client.is_closed():
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    print(current_time)
-    if Today == 0 and current_time == "01:00:00":
-      await channel.send(file=discord.File("AloneBot.png"))
+    if Today == 0 and current_time == "01:00:00": # Day: 0, 01:00:00, Saturday in Argentina
+      await idChannel.send(file=discord.File("AloneBot.png"))
     await asyncio.sleep(1)
 
 @client.event
@@ -35,19 +37,26 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  global idChannel
   if message.author == client.user:
     return
 
   mgs = message.content
-    
+
   if message.content.startswith('%Saturday'):
     await message.channel.send(file=discord.File("AloneBot.png"))
+  
+  if message.content.startswith('%general'):
+    idChannel = message.channel
+    print(idChannel)
+    await message.channel.send("Nice, I got it!")
 
   if any(word in mgs for word in Alone_words):
     await message.channel.send(random.choice(Alone_Answers))
 
 @client.event
 async def on_message_delete(message):
+  global idChannel
   await message.channel.send("Ha are you deleting a message?. God you are Pathetic.")
 
 client.loop.create_task(AloneOnSaturday())
